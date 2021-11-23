c72590da7041a20b06170aa669a7d4ad438a3913
Ayla1524 <aylasilva1524@outlook.com>
2021-11-22 23:52:16 -0300

    adicionando star-wars

---
+++ src/pages/CharacterListPages/index.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {CharacterCard , H1Card , ImageBackground , Title}  from "./styled"
+import {getCharacterList} from '../../service/request'
+
+
+const CharacterListPages = (props) => {
+  const [characterList, setCharacterList] = useState([]);
+
+
+  
+  useEffect(() => {
+    getCharacterList(setCharacterList);
+  }, []);
+
+  
+  
+  function personList () {
+    return characterList.map((character,index) => {
+      return < CharacterCard onClick={()=> props.goToDetailsPage(character.url)} 
+        key={index}>
+        {character.name}
+        </CharacterCard>
+  })
+
+  }
+
+  return (
+    <ImageBackground>
+      <Title> Star Wars</Title>
+      <H1Card>Lista de Personagens </H1Card>
+    {personList()}
+    </ImageBackground>
+  )
+
+
+};
+
+export default CharacterListPages;
